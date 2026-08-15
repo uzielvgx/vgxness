47a52df63ad45a3ab73543abb8372180fe0f3e6b
Uziel Vega <uzielvgx@MacBook-Pro-de-Uziel.local>
2026-08-14 22:40:11 -0600

    fix(installer): open Windows rename source natively

--- internal/selfinstall/publish_directory_windows.go
+++ internal/selfinstall/publish_directory_windows.go
@@ -18,22 +18,20 @@ type fileRenameInformation struct {
 	FileName        [1]uint16
 }
 
-var reOpenFile = windows.NewLazySystemDLL("kernel32.dll").NewProc("ReOpenFile")
-
 // publishRootDirectoryNoReplace renames through handles opened beneath root,
 // so a replacement of root.Name() cannot redirect the operation.
 func publishRootDirectoryNoReplace(root *os.Root, source, destination string) error {
-	parent, err := root.Open(filepath.Dir(destination))
+	destinationParent, err := root.Open(filepath.Dir(destination))
 	if err != nil {
 		return err
 	}
-	defer parent.Close()
-	sourceFile, err := root.Open(source)
+	defer destinationParent.Close()
+	sourceParent, err := root.Open(filepath.Dir(source))
 	if err != nil {
 		return err
 	}
-	defer sourceFile.Close()
-	handle, err := reopenForRename(windows.Handle(sourceFile.Fd()))
+	defer sourceParent.Close()
+	handle, err := openForRename(windows.Handle(sourceParent.Fd()), filepath.Base(source))
 	if err != nil {
 		return err
 	}
@@ -47,20 +45,30 @@ func publishRootDirectoryNoReplace(root *os.Root, source, destination string) er
 	size := int(unsafe.Offsetof(dummy.FileName)) + nameLength
 	buffer := make([]byte, size)
 	info := (*fileRenameInformation)(unsafe.Pointer(&buffer[0]))
-	info.RootDirectory = windows.Handle(parent.Fd())
+	info.RootDirectory = windows.Handle(destinationParent.Fd())
 	info.FileNameLength = uint32(nameLength)
 	copy((*[windows.MAX_LONG_PATH]uint16)(unsafe.Pointer(&info.FileName[0]))[:nameLength/2:nameLength/2], name)
 	var status windows.IO_STATUS_BLOCK
 	err = windows.NtSetInformationFile(handle, &status, &buffer[0], uint32(size), windows.FileRenameInformation)
-	runtime.KeepAlive(parent)
-	runtime.KeepAlive(sourceFile)
+	runtime.KeepAlive(destinationParent)
+	runtime.KeepAlive(sourceParent)
 	return err
 }
 
-func reopenForRename(handle windows.Handle) (windows.Handle, error) {
-	value, _, err := reOpenFile.Call(uintptr(handle), uintptr(windows.DELETE|windows.SYNCHRONIZE), uintptr(windows.FILE_SHARE_READ|windows.FILE_SHARE_WRITE), uintptr(windows.FILE_FLAG_BACKUP_SEMANTICS|windows.FILE_FLAG_OPEN_REPARSE_POINT))
-	if value == 0 {
+func openForRename(parent windows.Handle, name string) (windows.Handle, error) {
+	objectName, err := windows.NewNTUnicodeString(name)
+	if err != nil {
 		return 0, err
 	}
-	return windows.Handle(value), nil
+	attributes := windows.OBJECT_ATTRIBUTES{
+		Length:        uint32(unsafe.Sizeof(windows.OBJECT_ATTRIBUTES{})),
+		RootDirectory: parent,
+		ObjectName:    objectName,
+		Attributes:    windows.OBJ_CASE_INSENSITIVE,
+	}
+	var handle windows.Handle
+	var status windows.IO_STATUS_BLOCK
+	err = windows.NtCreateFile(&handle, windows.DELETE|windows.SYNCHRONIZE, &attributes, &status, nil, windows.FILE_ATTRIBUTE_NORMAL, windows.FILE_SHARE_READ|windows.FILE_SHARE_WRITE|windows.FILE_SHARE_DELETE, windows.FILE_OPEN, windows.FILE_OPEN_REPARSE_POINT|windows.FILE_OPEN_FOR_BACKUP_INTENT|windows.FILE_SYNCHRONOUS_IO_NONALERT, 0, 0)
+	runtime.KeepAlive(objectName)
+	return handle, err
 }
